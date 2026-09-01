d77d11be9b8bf49213ab3e9c666417ef2dada5e6
jhehanna26 <jhehannai@gmail.com>
2026-09-01 17:36:11 +0800

    Create Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class DisplayOutput {
+	public static void main(String[] args) {
+		
+		String FN = "Jhehanna"; //String for text
+		String LN = "Ignacio"; //String for text
+		String AD = "Manaoag,Pangasinan"; //String for text
+		String MB = "09657836004"; //String because it starts with 0
+		int age = 18; //int for whole numbers
+		int gwa = 94; //int for whole numbers
+		
+		System.out.println ("Firstname: " + FN); //Displays the firstname
+		System.out.println ("Lastname: " + LN); //Displays the lastname
+		System.out.println ("Address: " + AD); //Displays your Address
+		System.out.println ("Mobile Number: " + MB); //Output your mobile number
+		System.out.println ("Age: " + age); //Output your age
+		System.out.println ("GWA: " + gwa); //Output your gwa
+	}
+      }
