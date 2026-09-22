4c2a3da81fcdb3bb57d1f010a6e87a09209554d9
jhehanna26 <jhehannai@gmail.com>
2026-09-22 20:42:05 +0800

    Add files via upload

---
+++ firstJava.java
@@ -0,0 +1,9 @@
+public class firstJava {
+	public static void main(String[] args){ 
+	
+	String name = "Jhehanna S. Ignacio";
+	String sec = "BSIT 1A";
+	System.out.println("Name: " + name);
+	System.out.println("Section: " + sec);
+	}
+}
