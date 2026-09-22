906724139e88ab2234beaf07451f2656486b81e7
jhehanna26 <jhehannai@gmail.com>
2026-09-22 20:42:38 +0800

    Add files via upload

---
+++ secondJava.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		String name = "JHEHANNA S. IGNACIO";
+		String AD = "MANAOAG, PANGASINAN";
+		String CN = "09657836004";
+		int age = 18;
+		String crush = "Wala po";
+		System.out.println(name + "," + AD + "," + CN + "," + age + "," + crush);
+		}
+
+}
