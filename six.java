32113107c87017968a20d5040641031528d9a0f1
jhehanna26 <jhehannai@gmail.com>
2026-09-23 22:16:56 +0800

    Update six.java

--- six.java
+++ six.java
@@ -10,6 +10,6 @@ public class six {
         name = inputDevice.nextLine();
         System.out.print("Please enter your age ");
                 age = inputDevice.nextInt();
-        System.out.print("Your name is " + name + " and you are " + age + " years old.");
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
     }
 }
